8d95905dce920fde91b6fef60fb562496ac398dd
Adilbek11kz <tadilbek11kz@gmail.com>
2020-10-14 17:15:40 +0600

    init: create reverse-int project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let output = 0;
+    n = n < 0 ? n * -1: n;
+    while (n != 0) {
+        output *= 10;
+        output += (n % 10);
+        n = (n - (n % 10)) / 10
+    }
+    return output;
 }
